571541d44af6c4f54bc366dd7f1eef49dee177aa
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2023-10-27 03:46:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let withoutArticles = [];
 let map = {};
 for(let i=0; i<arr.length; i++){
 	let str = arr[i];
-	str = str.reeplace(/\ban\b|\bthe\b|\ba\b/gi, "");
+	str = str.reeplace(/\ban\b|\bthe\b|\ba\b/gi, "")
 	str = str.reeplace(/ +/gi, " ");
 	str = str.trim();
 	withoutArticles.push(str);
